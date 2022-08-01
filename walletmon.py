e8b224aaa6fdca854ebd6e4213b3b1b47aa66ef4
Relyte <96859270+Relyte@users.noreply.github.com>
2022-08-01 14:38:45 -0400

    Initial Commit

---
+++ walletmon.py
@@ -0,0 +1,98 @@
+#! /usr/bin/python3
+
+import argparse
+import subprocess
+import json
+import ast
+from discord_webhook import DiscordWebhook
+import re
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-a",
+        "--addresses",
+        nargs='+',
+        dest="addresses",
+        type=str,
+        required=True,
+        help="Addresses to check balances"
+    )
+    parser.add_argument(
+        "-d",
+        "--discord",
+        dest="discord_webhook_url",
+        type=str,
+        required=True,
+        help="Discord webhook url to send notifications to"
+    )
+
+    args = parser.parse_args()
+
+    if args.discord_webhook_url:
+        global discord_webhook_url
+        discord_webhook_url=args.discord_webhook_url
+        print(discord_webhook_url)
+
+    wallets=[]
+    if args.addresses:
+        for address in args.addresses:
+            wallets.append(map_address_to_chain(address))
+    #wallets.append(map_address_to_chain('kujira1eywufmvkl4805stahfkdt0qetc3r0zvh98qzpz'))
+    print(wallets)
+    for wallet in wallets:
+        wallet['balance']=check_balance(wallet)
+        print(wallet)
+        check_threshold(wallet)
+
+def map_address_to_chain(address):
+    pattern = re.compile(r"""(?x)
+    (?P<kujira>^kujira.*) |
+    (?P<clan>^clan.*) |
+    (?P<secret>^secret.*) |
+    (?P<cosmos>^cosmos.*)
+    """)
+    mo=pattern.fullmatch(address)
+    match mo.lastgroup:
+        case 'kujira':
+            return {'address':address,
+                    'chain': 'kujira',
+                    'daemon': 'kujirad',
+                    'denom': 'ukuji'}
+        case 'clan':
+            print("This is a clan address")
+        case 'secret':
+            print("This is a secret address")
+        case 'cosmos':
+            print("This is a cosmos address")
+        case _:
+            raise ValueError(f'Unknown pattern for {s!r}')
+
+def check_balance(wallet=dict):
+    query=subprocess.run([wallet['daemon'], "query", "bank", "balances", wallet['address'], "--node", f"https://rpc.cosmos.directory:443/{wallet['chain']}", "--denom", wallet['denom'], "--output", "json"], universal_newlines=True, capture_output=True)
+    results=json.loads(query.stdout)
+    balance = (results['amount'])
+    print(f"Wallet Balance for {wallet['chain']}: {balance}{wallet['denom']}")
+    return balance
+
+def send_discord_message(content=str,discord_webhook_url=str):
+    webhook = DiscordWebhook(url=discord_webhook_url, content=content)
+    response = webhook.execute()
+
+def check_threshold(wallet=dict):
+    print("Matching Threshold")
+    match wallet['denom']:
+        case 'ukuji':
+            threshold=1000000
+        case 'uscrt':
+            threshold=10000000
+    if int(wallet['balance']) < threshold:
+        content=""
+        content+=f"{wallet['address']}: {wallet['balance']}{wallet['denom']}"
+        print(content)
+        print("Calling send_discord_message")
+        send_discord_message(content,discord_webhook_url)
+
+if __name__ == "__main__":
+    main()
