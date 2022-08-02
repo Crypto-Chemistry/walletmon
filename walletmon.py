cd3df3b09a12268f16b595f77a32e26841aca7f9
Relyte <96859270+Relyte@users.noreply.github.com>
2022-08-02 14:18:00 -0400

    Add chain-registry support. Remove old manual matching

--- walletmon.py
+++ walletmon.py
@@ -7,6 +7,9 @@ import ast
 from discord_webhook import DiscordWebhook
 import re
 import requests
+import git
+import pathlib
+import os
 
 def main():
     parser = argparse.ArgumentParser()
@@ -34,39 +37,88 @@ def main():
     if args.discord_webhook_url:
         global discord_webhook_url
         discord_webhook_url=args.discord_webhook_url
-        print(discord_webhook_url)
+
+    update_chain_registry()
 
     wallets=[]
     if args.addresses:
         for address in args.addresses:
             wallets.append(map_address_to_chain(address))
-    print(wallets)
+    else:
+        print("Error - Please supply addresses to monitor using the '-a' flag")
+        quit()
+
     for wallet in wallets:
         wallet['balance']=check_balance(wallet)
         check_threshold(wallet)
 
+def update_chain_registry():
+    git_url="https://github.com/Crypto-Chemistry/tenderduty_config_updater.git"
+    repo_dir="chain-registry"
+    script_dir=pathlib.Path( __file__ ).parent.absolute()
+    repo_path=os.path.join(script_dir,repo_dir)
+    if not os.path.exists(repo_path):
+        git.Repo.clone_from(git_url, repo_path)
+    repo = git.Repo(repo_path)
+    origin = repo.remotes.origin
+    origin.pull()
+
 def map_address_to_chain(address):
-    pattern = re.compile(r"""(?x)
-    (?P<kujira>^kujira.*) |
-    (?P<clan>^clan.*) |
-    (?P<secret>^secret.*) |
-    (?P<cosmos>^cosmos.*)
-    """)
-    mo=pattern.fullmatch(address)
-    match mo.lastgroup:
-        case 'kujira':
-            return {'address':address,
-                    'chain': 'kujira',
-                    'daemon': 'kujirad',
-                    'denom': 'ukuji'}
-        case 'clan':
-            print("This is a clan address")
-        case 'secret':
-            print("This is a secret address")
-        case 'cosmos':
-            print("This is a cosmos address")
-        case _:
-            raise ValueError(f'Unknown pattern for {s!r}')
+    repo_dir="chain-registry"
+    script_dir=pathlib.Path( __file__ ).parent.absolute()
+    repo_path=os.path.join(script_dir,repo_dir)
+    
+    chain_json=find_chain_json(address)
+    f = open(chain_json)
+    chain_data=json.load(f)
+
+    asset_json=chain_json.replace("chain.json","assetlist.json")
+    f = open(asset_json)
+    asset_data=json.load(f)
+
+    #Get chain name from chain.json
+    try:
+        chain_name=chain_data['chain_name']
+    except KeyError as e:
+        print("KeyError - Chain name likely does not exist in chain-registry")
+        quit()
+
+    #Get base denom from assetlist.json
+    try:
+        chain_base_denom=asset_data['assets'][0]['base']
+    except KeyError as e:
+        print("KeyError - Fee denomination likely does not exist in chain-registry")
+        quit()
+
+    return {'address':address,
+            'chain': chain_name,
+            'denom': chain_base_denom}
+
+def find_chain_json(address):
+    repo_dir="chain-registry"
+    script_dir=pathlib.Path( __file__ ).parent.absolute()
+    repo_path=os.path.join(script_dir,repo_dir)
+    filenames = []
+
+    #Retrieve all json files and add to array
+    testnet_folder="testnets"
+    for root, dirs, files in os.walk(repo_path):
+        for file in files:
+            if file.endswith(".json") and testnet_folder not in root:
+                filenames.append(os.path.join(root, file))
+
+    #Get HRP of address
+    hrp=re.split('1',address)[0]
+
+    #Match the prefix and return the file name
+    pattern = re.compile(f'\"bech32_prefix\":\s\"{hrp}\"')
+    chain_json=""
+    for file in filenames:
+        for i, line in enumerate(open(file)):
+            for match in re.finditer(pattern, line):
+                chain_json=file
+                break
+    return chain_json
 
 def check_balance(wallet=dict):
     url=f"https://rest.cosmos.directory/{wallet['chain']}/cosmos/bank/v1beta1/balances/{wallet['address']}/by_denom?denom={wallet['denom']}"
@@ -81,16 +133,16 @@ def send_discord_message(content=str,discord_webhook_url=str):
     response = webhook.execute()
 
 def check_threshold(wallet=dict):
-    print("Matching Threshold")
+    #TODO: Add thresholds that are customizable (dynamic?) and set sane defaults
     match wallet['denom']:
         case 'ukuji':
             threshold=1000000
         case 'uscrt':
             threshold=10000000
+        case _:
+            threshold=10000000
     if int(wallet['balance']) < threshold:
         content=f"{wallet['address']}: {wallet['balance']}{wallet['denom']}"
-        print(content)
-        print("Calling send_discord_message")
         send_discord_message(content,discord_webhook_url)
 
 if __name__ == "__main__":
